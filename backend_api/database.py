559310f39f6a1b079d57f52a13cd249b932b76b2
Marcin Rogalski <iwannabe.dev@gmail.com>
2023-10-01 01:31:17 +0200

    Prevent config data from leak
    Used 'decouple' library to hide sensitive, configuration data.

--- backend_api/database.py
+++ backend_api/database.py
@@ -1,48 +1,48 @@
 import psycopg2
 from psycopg2 import OperationalError
-from psycopg2 import sql
+from decouple import config
 
 
 class DatabaseError(Exception):
     pass
 
+
 class Database:
-    def __init__(self, database, user, password, host, port):
-        self.database = database
-        self.user = user
-        self.password = password
-        self.host = host
-        self.port = port
-        self.conn = None
+    # def __init__(self, database, user, password, host, port):
+    def __init__(self, db_name=None):
+        self.user = config('USERNAME')
+        self.password = config('PASSWORD')
+        self.host = config('HOST')
+        self.port = config('PORT')
+        self.db_name = db_name or config('DB_NAME')
+        self.conn = self.connect()
         self.cur = None
 
     def connect(self):
-        if self.conn is None:
-            try:
-                self.conn = psycopg2.connect(
-                    database=self.database,
-                    user=self.user,
-                    password=self.password,
-                    host=self.host,
-                    port=self.port
-                )
-                self.conn.autocommit = True
-                self.cur = self.conn.cursor()
-            except psycopg2.DatabaseError:
-                raise DatabaseError('Error connecting to the database.')
+        connection = None
+        try:
+            connection = psycopg2.connect(
+                user=self.user,
+                password=self.password,
+                host=self.host,
+                port=self.port,
+                database=self.db_name
+            )
+            connection.autocommit = True
+        except psycopg2.DatabaseError:
+            raise DatabaseError('Error connecting to the database.')
 
-        return self.conn
+        return connection
 
     def execute_query(self, query, parameters=None):
-        cursor = self.cur
+        cursor = self.conn.cursor()
         try:
             cursor.execute(query, parameters)
-            # print(f"Query '{query}' executed successfully")
             return cursor
         except OperationalError as e:
-            raise DatabaseError('DB error.')
+            raise DatabaseError(f'DB error. {e}')
         except psycopg2.errors.UniqueViolation as pg_err:
-            raise DatabaseError('DB error.')
+            raise DatabaseError(f'DB error. {pg_err}')
 
     def database_exists(self, db_name):
         database_list_query = "SELECT datname FROM pg_database;"
@@ -57,17 +57,4 @@ class Database:
             return False
 
         except OperationalError as e:
-            raise DatabaseError('DB error.')
-
-
-# db = Database("codecrafters", "testuser", "1234", "localhost", "5432")
-# db.connect()
-
-# print(db.database_exists("postgres"))
-# print(db.execute_query("SELECT * FROM users").fetchall())
-
-
-# email = "anna@hotmail.com"
-# query = sql.SQL("SELECT email, password, user_type FROM users WHERE email = {}").format(sql.Literal(email))
-# result = db.execute_query(query).fetchone()
-# print(result[0])
+            raise DatabaseError(f'DB error.{e}')
